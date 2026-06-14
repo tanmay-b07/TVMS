e27d5b474e6abdcba63a28f6249875827e65627f
tanmay-b07 <tanmaybhosle572@gmail.com>
2026-06-14 22:25:21 +0530

    feat: add ESP32 MQTT gateway source

---
+++ esp32/README.md
@@ -0,0 +1,19 @@
+# ESP32 MQTT Gateway
+
+This directory contains the ESP32 firmware used as the communication gateway in the TVMS project.
+
+## Responsibilities
+
+* Receive sensor data from STM32 through UART
+* Connect to WiFi
+* Publish data to HiveMQ Cloud using MQTT
+
+## Development Environment
+
+* Arduino IDE
+* ESP32 Board Package
+
+## Version
+
+TVMS V1.0
+
---
+++ esp32/mqtt_publisher.ino
@@ -0,0 +1,93 @@
+#include <WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+
+#define RXD2 16
+const char* ssid = "your_wifi_name";
+const char* password = "your_wifi_password";
+
+const char* mqtt_server = "mqtt_server_url";
+const char* mqtt_user = "mqtt_username";
+const char* mqtt_pass = "mqtt_password";
+
+WiFiClientSecure espClient;
+PubSubClient client(espClient);
+
+String data = "";
+
+void connectMQTT()
+{
+  while (!client.connected())
+  {
+    Serial.print("Connecting MQTT... ");
+
+    if (client.connect("ESP32_Client", mqtt_user, mqtt_pass))
+    {
+      Serial.println("Connected");
+    }
+    else
+    {
+      Serial.println("Failed");
+      delay(2000);
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, -1);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("\nWiFi Connected");
+
+  espClient.setInsecure();
+
+  client.setServer(mqtt_server, 8883);
+
+  connectMQTT();
+}
+
+void loop()
+{
+  if (!client.connected())
+  {
+    connectMQTT();
+  }
+
+  client.loop();
+
+  while (Serial2.available())
+  {
+    char c = Serial2.read();
+
+    if (c == '\n')
+    {
+      data.trim();
+
+      if (data.length() > 0)
+      {
+        Serial.print("Publishing: ");
+        Serial.println(data);
+
+        client.publish(
+          "your_mqtt_topic",
+          data.c_str()
+        );
+      }
+
+      data = "";
+    }
+    else
+    {
+      data += c;
+    }
+  }
+}
